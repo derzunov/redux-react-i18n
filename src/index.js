866d29c25e1a9d4103383e630fa3111aa912ab04
derzunov <derzunov.work@gmail.com>
2016-11-01 22:14:12 +0300

    re-export modules fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import * as i18nActions from './actions';
-import i18nReducer from './reducer';
+export * as i18nActions from './actions';
+export { default as i18nReducer } from './reducer'
 
 // You can use the complete scheme of store/reducers/actions proposed by the author
-import Loc from './Loc';
+export { default as Loc } from './Loc';
 
 // Or you can just take a presentational container and map state to props by yourself
-import LocPresentational from './LocPresentational';
-
-export default { i18nActions, i18nReducer, Loc, LocPresentational }
+export { default as LocPresentational } from './LocPresentational';
