e0812779ab93bdcd4432ede30b2d4fee0acd2b87
derzunov <derzunov.work@gmail.com>
2016-11-02 00:02:03 +0300

    spaces

--- src/reducer.js
+++ src/reducer.js
@@ -52,11 +52,11 @@ const i18n = ( state, action ) => {
                     dictionaries: {
                         'ru-RU': {
                             'key_1': 'Первый дефолтный ключ',
-                            'key_2': [ '$Count', ' ', ['штучка','штучки','штучек']], // 1 штучка, 3 штучки, пять штучек
+                            'key_2': [ '$Count', ' ', [ 'штучка','штучки','штучек' ] ], // 1 штучка, 3 штучки, пять штучек
                         },
                         'en-US': {
                             'key_1': 'First default key',
-                            'key_2': [ '$Count', ' ', ['thing','things']], // 1 thing, 2 things, 153 things
+                            'key_2': [ '$Count', ' ', [ 'thing','things' ] ], // 1 thing, 2 things, 153 things
                         }
                     }
             };
