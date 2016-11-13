6fa31101a221c5a67a7a7814ff1c6c2dd12e2e65
derzunov <derzunov.work@gmail.com>
2016-11-13 22:29:59 +0300

    delete "the"

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import * as i18nActions from './actions';
 export { i18nActions };
 export { default as i18nReducer } from './reducer'
 
-// You can use the complete scheme of store/reducers/actions proposed by the author
+// You can use the complete scheme of store/reducers/actions proposed by author
 export { default as Loc } from './Loc';
 
 // Or you can just take a presentational container and map state to props by yourself
