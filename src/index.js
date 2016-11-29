68ec85d88d1c1eae56977238dfb9f0c658cc209d
derzunov <derzunov.work@gmail.com>
2016-11-29 22:31:36 +0300

    move React Native i18n to another package/repo

--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ export { default as i18nReducer } from './reducer'
 
 // You can use the complete scheme of store/reducers/actions proposed by author
 export { default as Loc } from './Loc'
-// React Native Component
-export { default as LocRN } from './LocRN'
 
 // Or you can just take a presentational container and map state to props by yourself
 export { default as LocPresentational } from './LocPresentational'
-
-// presentational React Native Component
-export { default as LocPresentationalRN } from './LocPresentationalRN'
