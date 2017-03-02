e24f324696f5c7d357abbd9ccc05802f5381ee5a
Dmitry Erzunov <derzunov.work@gmail.com>
2017-03-02 22:20:14 +0300

    Update Form.js

--- example/src/components/Form.js
+++ example/src/components/Form.js
@@ -1,36 +1,22 @@
-import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
+import React, { Component } from 'react'
+import { Field, reduxForm } from 'redux-form'
 import Count from './Count'
 
-let normalize = (value) => {
-    return Math.abs( value );
-};
-
-// class CountForm extends Component {
-//     render() {
-//         return (
-//             <form>
-//                 <div>
-//                     <Field normalize={normalize} name="count" component={Count}/>
-//                 </div>
-//             </form>
-//         );
-//     }
-// }
-
-
+let normalize = ( value ) => {
+    return Math.abs( value )
+}
 
 const CountFormComponent = () => {
     return (<form>
         <div>
             <Field normalize={normalize} name="count" component={Count}/>
         </div>
-    </form>);
+    </form>)
 };
 
 // Decorate the form component
 const CountForm = reduxForm({
     form: 'countForm' // a unique name for this form
-})(CountFormComponent);
+})( CountFormComponent )
 
-export default CountForm;
+export default CountForm
