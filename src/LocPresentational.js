39d9c14c563d1793611033b6b933eff9904628b4
Vladi Rachlin <vladi@colu.com>
2019-10-29 18:26:10 +0200

    Use React.Fragment instead of span

--- src/LocPresentational.js
+++ src/LocPresentational.js
@@ -1,10 +1,8 @@
 import translate from 'translatr'
 import React from 'react'
 
-const Loc = ( { currentLanguage, locKey, number, dictionary, dispatch, ...rest } ) => {
-    return <span { ...rest }>
-        { translate( dictionary, currentLanguage, locKey, number ) }
-    </span>
+const Loc = ({ currentLanguage, locKey, number, dictionary, dispatch, ...rest }) => {
+  return <React.Fragment {...rest}>{translate(dictionary, currentLanguage, locKey, number)}</React.Fragment>
 }
 
 export default Loc
